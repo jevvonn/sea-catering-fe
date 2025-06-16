0e398203ed30faee342e593b8920007b49c34caa
jevvonn <darmawanb985@gmail.com>
2025-06-17 01:41:06 +0700

    feat: implement TestimonialsSection with testimonial submission form and rating input

--- components/landing-page/testimonials.tsx
+++ components/landing-page/testimonials.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "../ui/textarea";
+import RatingInput from "../ui/rating-input";
 
 const testimonials: {
   quote: string;
@@ -70,23 +77,76 @@ const testimonials: {
 
 const TestimonialsSection = () => {
   return (
-    <section className="mt-4">
-      <h2 className="text-3xl font-semibold text-center">Testimonials</h2>
+    <>
+      <section className="mt-10">
+        <h2 className="text-3xl font-semibold text-center">
+          What People Say ?
+        </h2>
 
-      <div className="mt-10 cursor-default">
-        <InfiniteMovingCards
-          items={testimonials.filter((_, idx) => idx < 5)}
-          direction="right"
-          speed="slow"
-        />
-        <InfiniteMovingCards
-          items={testimonials.filter((_, idx) => idx > 5)}
-          direction="left"
-          speed="slow"
-        />
-      </div>
-    </section>
+        <div className="mt-10 cursor-default">
+          <InfiniteMovingCards
+            items={testimonials.filter((_, idx) => idx < 5)}
+            direction="right"
+            speed="slow"
+          />
+          <InfiniteMovingCards
+            items={testimonials.filter((_, idx) => idx > 5)}
+            direction="left"
+            speed="slow"
+          />
+        </div>
+      </section>
+
+      <section className="mt-10 pb-10">
+        <h2 className="text-3xl font-semibold text-center">
+          Share Your Experience
+        </h2>
+
+        <TestimonialForm className="mt-10" />
+      </section>
+    </>
   );
 };
+
+function TestimonialForm({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex justify-center ", className)} {...props}>
+      <Card className="shadow-none border-0 md:border p-0 w-full md:w-3/4 lg:w-1/2">
+        <CardContent className="grid p-0">
+          <form className="md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  type="name"
+                  placeholder="Jevon Mozart"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="message">Message</Label>
+                <Textarea
+                  id="message"
+                  className="resize-none min-h-[200px]"
+                  placeholder="Share your experience with SEA Catering..."
+                  required
+                />
+              </div>
+
+              <div className="grid gap-3">
+                <Label htmlFor="message">Rating</Label>
+                <RatingInput maxRating={5} />
+              </div>
+              <Button type="submit" className="w-full">
+                Send Testimonial
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
 
 export default TestimonialsSection;
