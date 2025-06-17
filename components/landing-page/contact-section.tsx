ff355990bee8a2fa13dceccc2b7de8567ef375be
jevvonn <darmawanb985@gmail.com>
2025-06-18 00:40:50 +0700

    fix: add mx-auto class to container in ContactSection for improved centering

--- components/landing-page/contact-section.tsx
+++ components/landing-page/contact-section.tsx
@@ -10,7 +10,7 @@ import {
 const ContactSection = () => {
   return (
     <section className="w-full py-12 mb-12 rounded-md">
-      <div className="container px-4 md:px-6">
+      <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
