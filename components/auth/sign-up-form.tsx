446427d9754317e38ed32b51d7a3a86988608da5
jevvonn <darmawanb985@gmail.com>
2025-06-26 19:54:33 +0700

    feat: implement sign-up form with validation using Zod and React Hook Form

--- components/auth/sign-up-form.tsx
+++ components/auth/sign-up-form.tsx
@@ -1,57 +1,168 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
+const formSchema = z
+  .object({
+    name: z.string().min(3, {
+      message: "Fullname must be at least 3 characters.",
+    }),
+    email: z.string().email({
+      message: "Invalid email address.",
+    }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters." })
+      .regex(/[a-z]/, { message: "Password must include a lowercase letter." })
+      .regex(/[A-Z]/, { message: "Password must include an uppercase letter." })
+      .regex(/[0-9]/, { message: "Password must include a number." })
+      .regex(/[^A-Za-z0-9]/, {
+        message: "Password must include a special character.",
+      }),
+    confirmPassword: z.string().min(8, {
+      message: "Confirm Password must be at least 8 characters.",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
 
 const SignUpForm = ({ className, ...props }: React.ComponentProps<"form">) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Sign Up New Account</h1>
-      </div>
-      <div className="grid gap-6">
-        <div className="grid gap-3">
-          <Label htmlFor="full-name">Full Name</Label>
-          <Input
-            id="full-name"
-            type="text"
-            placeholder="Jevon Mozart"
-            required
-          />
+    <Form {...form}>
+      <form
+        className={cn("flex flex-col gap-6", className)}
+        {...props}
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Sign Up New Account</h1>
         </div>
-        <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="jevon@gmail.com"
-            required
-          />
-        </div>
-        <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
+        <div className="grid gap-6">
+          <div className="grid gap-3">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex-1 w-full md:w-auto">
+                  <FormLabel>Full Name</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input placeholder="Jevon Mozart" required {...field} />
+                    </div>
+                  </FormControl>
+                  <FormMessage className="text-sm" />
+                </FormItem>
+              )}
+            />
           </div>
-          <Input id="password" type="password" required />
-        </div>
-        <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="confirm-password">Confirm Password</Label>
+          <div className="grid gap-3">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex-1 w-full md:w-auto">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        placeholder="jevon@gmail.com"
+                        type="email"
+                        required
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage className="text-sm" />
+                </FormItem>
+              )}
+            />
           </div>
-          <Input id="confirm-password" type="password" required />
+          <div className="grid gap-3">
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex-1 w-full md:w-auto">
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        placeholder="*******"
+                        type="password"
+                        required
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage className="text-sm" />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="grid gap-3">
+            <FormField
+              name="confirmPassword"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex-1 w-full md:w-auto">
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        placeholder="*******"
+                        type="password"
+                        required
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage className="text-sm" />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
         </div>
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-      </div>
-      <div className="text-center text-sm">
-        Already have an account?{" "}
-        <Link href="/sign-in" className="underline underline-offset-4">
-          Sign in
-        </Link>
-      </div>
-    </form>
+        <div className="text-center text-sm">
+          Already have an account?{" "}
+          <Link href="/sign-in" className="underline underline-offset-4">
+            Sign in
+          </Link>
+        </div>
+      </form>
+    </Form>
   );
 };
 
